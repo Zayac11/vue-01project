452ac9624e821f4ea0d35f1e30238949993db8e4
Zayac11 <brunogrbstudios@gmail.com>
2021-04-13 17:18:06 +0300

    solved first lesson + homework

---
+++ src/components/Counter.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div>
+            Введите количество продуктов:
+        </div>
+        <button v-on:click="decrementFunction">-</button>
+        <input type="number" @input="numberInput($event.target.value)" v-model="count">
+        <button v-on:click="incrementFunction">+</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        numberInput(value) {
+            this.count = value
+        },
+        incrementFunction() {
+            if(this.count<10) this.count++
+        },
+        decrementFunction() {
+            if(this.count>0) this.count--
+        },
+    }
+}
+</script>
+
+<style>
+    /*.container {*/
+    /*    width: 1200px;*/
+    /*    margin: 20px auto;*/
+    /*}*/
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div>
+            <input type="text" @input="emailInput($event.target.value)" :input="name">
+        </div>
+        <div>
+            <input type="password" v-on:input="passwordInput" :input="password">
+        </div>
+        <div>
+            <div>{{name}}</div>
+            <div>{{password}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            name: '',
+            password: '',
+        }
+    },
+    methods: {
+        emailInput(value) {
+            this.name = value
+        },
+        passwordInput(e) {
+            this.password = e.target.value
+        },
+    }
+}
+</script>
+
+<style>
+    .container {
+        width: 1200px;
+        margin: 20px auto;
+    }
+</style>
